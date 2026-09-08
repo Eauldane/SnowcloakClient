ea2a32f3a8f2632741f54e901d9dc01193aaf4e3
Eauldane <elf@eauldane.com>
2026-09-08 23:53:17 +0100

    BST hotfix

--- Snowcloak/Services/Pairing/PairingAvailabilityStore.cs
+++ Snowcloak/Services/Pairing/PairingAvailabilityStore.cs
@@ -545,6 +545,7 @@ internal sealed class PairingAvailabilityStore : Store<AvailabilityViewState>, I
         22 => "DRG", 23 => "BRD", 24 => "WHM", 25 => "BLM", 26 => "ACN", 27 => "SMN", 28 => "SCH",
         29 => "ROG", 30 => "NIN", 31 => "MCH", 32 => "DRK", 33 => "AST", 34 => "SAM", 35 => "RDM",
         36 => "BLU", 37 => "GNB", 38 => "DNC", 39 => "RPR", 40 => "SGE", 41 => "VPR", 42 => "PCT",
+        43 => "BST",
         _ => "-",
     };
 
